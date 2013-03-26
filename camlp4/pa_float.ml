f18c1cc770adfb2ce0169df47abb51534e5ad6ed
Jason Yeo <jasonyeo88@gmail.com>
2013-03-27 01:23:42 +0800

    added Float syntax extension

---
+++ camlp4/pa_float.ml
@@ -0,0 +1,44 @@
+(*
+  Code from pa_float camlp4 tutorial http://brion.inria.fr/gallium/index.php/Pa_float
+  Creates a syntax extension that converts integers and integer operators to
+  floating point operators. Float.(<expr>) will give <expr> with operators
+  transformed to floating point operators.
+
+  compile with ocamlc -o pa_float.cmo -I +camlp4 camlp4lib.cma -pp camlp4of.opt  -c pa_float.ml
+*)
+module Id = struct
+  let name = "pa_float"
+  let version = "1.0"
+end
+
+open Camlp4
+
+(* declare a functor Make *)
+module Make (Syntax : Sig.Camlp4Syntax) = struct
+  open Sig
+  include Syntax
+
+  class ['a] float_subst _loc = object
+    inherit Ast.map as super
+    method _Loc_t (_ : 'a) = _loc
+    method expr = function
+    | <:expr< ( + ) >> -> <:expr< ( +. ) >>
+    | <:expr< ( - ) >> -> <:expr< ( -. ) >>
+    | <:expr< ( * ) >> -> <:expr< ( *. ) >>
+    | <:expr< ( / ) >> -> <:expr< ( /. ) >>
+    | <:expr< $int:i$ >> ->
+      let f = float(int_of_string i) in <:expr< $`flo:f$ >>
+    | e -> super#expr e
+  end;;
+  EXTEND Gram
+    GLOBAL: expr;
+
+    expr: LEVEL "simple"
+      [ [ "Float"; "."; "("; e = SELF; ")" -> (new float_subst _loc)#expr e]
+      ]
+      ;
+    END
+
+end
+
+let module M = Register.OCamlSyntaxExtension(Id)(Make) in ()
